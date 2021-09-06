228884edfc2b218d50dcb14955e8da2d8d05cfbc
rBurgett <ryan@burgettdev.net>
2021-09-06 05:44:24 -0400

    Added comments to exampls.js

--- example.js
+++ example.js
@@ -3,14 +3,21 @@ const { blockMainnet } = require('./dist/networks/block');
 
 (async function() {
   try {
+
+    // create xrouter client
     const client = new XRouter({
       network: blockMainnet,
     }, console.log, console.error);
+
+    // start the server & wait for it to find two or more EXR nodes
     const started = await client.start();
     if(!started)
       return;
+
+    // make an spv call
     const blockCount = await client.getBlockCount('BLOCK');
     console.log(`block count is ${blockCount}`);
+
   } catch(err) {
     console.error(err);
   }
